05dc1080b4ecf8b6f3d4e5570398913f480ef5be
IceLocke <lockeice@gmail.com>
2024-03-28 00:11:50 +0800

    Merge pull request #2 from PRProber/ice-dev
    添加 upload_token，代码框架

--- backend/util/rating.py
+++ backend/util/rating.py
@@ -7,6 +7,25 @@ def single_rating(level: float, acc: float) -> float:
     """
     # TODO: Apply the formula.
     # Reference: https://www.bilibili.com/read/cv29433852
-    rating: float = 0
+    rating: float
+
+    bounds = [0.9, 0.93, 0.95, 0.97, 0.98, 0.99, 1]
+    rewards = [0.06, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02]
+
+    reward_rating: float = 0
+    for i, bound in enumerate(bounds):
+        if acc >= bound:
+            reward_rating += rewards[i]
+
+    if acc > 1:
+        rating = 0.2 * (level + 200.0 * (acc - 1.0) / 3.0)
+    else:
+        rating = 0.2 * (level * (acc ** 1.5) - 0.9) + reward_rating
+
+    int_rating: int = int(rating * 10000)
+    if int_rating % 2 != 0:
+        int_rating -= 1
+
+    rating = int_rating / 10000
 
     return rating
