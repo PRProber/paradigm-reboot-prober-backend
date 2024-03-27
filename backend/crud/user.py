741b8526e1bacaa5f3454d985b6e86f9f0480df2
IceLocke <lockeice@gmail.com>
2024-03-27 19:41:27 +0800

    添加 JWT 认证

--- backend/crud/user.py
+++ backend/crud/user.py
@@ -1,7 +1,37 @@
+from typing import Type
+
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
-from backend.model import entities, schemas
+
+from ..model import entities, schemas
+from ..model.entities import User
+from ..util import security
 
 
-def get_user(db: Session, username: str):
-    db_user = db.query(entities.User).filter(entities.User.username == username).first()
+def get_user(db: Session, username: str) -> User | None:
+    db_user = db.query(User).filter(entities.User.username == username).first()
+    return db_user
 
+
+def create_user(db: Session, user: schemas.UserCreate) -> User | None:
+    db_user = db.query(User).filter(User.username == user.username).first()
+    if db_user:
+        return None
+
+    db_user = entities.User(
+        username=user.username,
+        encoded_password=security.encode_password(user.password),
+        email=user.email,
+        qq_number=user.qq_number,
+        account=user.account,
+        account_number=user.account_number,
+        uuid=user.uuid,
+        anonymous_probe=user.anonymous_probe,
+        is_active=True,
+        is_admin=False
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
---
+++ backend/util/security.py
@@ -0,0 +1,70 @@
+from typing import Optional, Union
+from datetime import datetime, timedelta, timezone
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
+from jose import JWTError, jwt
+
+from ..model.schemas import User
+from .. import config
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='user/login')
+pwd_context = CryptContext(schemes=['bcrypt'])
+
+bad_credential_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Could not validate credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+
+def verify_password(plain_password: str, encoded_password: str):
+    return pwd_context.verify(plain_password, encoded_password)
+
+
+def encode_password(password: str | bytes):
+    return pwd_context.hash(password)
+
+
+def generate_jwt(data: dict, expires_delta: Union[timedelta, None] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=30)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, config.SECRETE_KEY, algorithm=config.JWT_ENCODE_ALGORITHM)
+    return encoded_jwt
+
+
+def generate_access_jwt(username: str, expires_delta: Union[timedelta, None] = None):
+    """
+    Invoked by service.login().
+    :param username:
+    :param expires_delta:
+    :return:
+    """
+    return generate_jwt(data={"sub": username}, expires_delta=expires_delta)
+
+
+def extract_payloads(token: str):
+    payload = jwt.decode(token, config.SECRETE_KEY, algorithms=[config.JWT_ENCODE_ALGORITHM])
+    return payload
+
+
+def extract_username(token: str) -> str:
+    """
+    Extract username from a JWT. If bad credential found (i.e. bad token or no corresponding field),
+    a 401 HTTP Exception will be raised.
+    :param token: JWT.
+    :return: username: extracted username.
+    """
+    try:
+        payload = extract_payloads(token)
+        username: str = payload.get("sub")
+        if username is None:
+            raise bad_credential_exception
+        return username
+    except JWTError:
+        raise bad_credential_exception
