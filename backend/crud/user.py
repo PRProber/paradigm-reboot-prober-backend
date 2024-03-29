fb07f827610d5681c52462573b3cfc57692be507
IceLocke <lockeice@gmail.com>
2024-03-29 16:06:20 +0800

    添加 crud.user 中添加游玩记录和获取游玩记录 / b50 的函数

--- backend/crud/user.py
+++ backend/crud/user.py
@@ -1,11 +1,14 @@
 import secrets
-from typing import Tuple, List
+from typing import Type, List
+from datetime import datetime
 
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import AmbiguousForeignKeysError
 
 from ..model import entities, schemas
-from ..model.entities import User
-from ..util import security
+from ..model.entities import User, PlayRecord
+from ..util import security, rating
 
 
 def get_user(db: Session, username: str) -> User | None:
@@ -38,33 +41,65 @@ def create_user(db: Session, user: schemas.UserCreate) -> User | None:
     return db_user
 
 
-def create_record(db: Session, record: schemas.PlayRecordCreate, user: User):
+def create_record(db: Session, record: schemas.PlayRecordCreate) -> entities.PlayRecord:
     """
     Create a play record.
     :param db: SQLAlchemy.orm Session
     :param record: record details
-    :param user: validated user
     :return: record entity
     """
     # TODO: Implement this function
-    pass
+    db_song_level: entities.SongLevel | None \
+        = (db.query(entities.SongLevel).filter(entities.SongLevel.song_level_id == record.song_level_id).first())
+    if db_song_level is None:
+        raise HTTPException(status_code=400, detail="Song level doesn't exist")
+
+    db_record = entities.PlayRecord(
+        song_level_id=record.song_level_id,
+        record_time=datetime.now(),
+        username=record.username,
+        score=record.score,
+        rating=rating.single_rating(db_song_level.level, record.score),
+    )
+    try:
+        db.add(db_record)
+        db.commit()
+        db.refresh(db_record)
+        return db_record
+    except AmbiguousForeignKeysError:
+        raise HTTPException(status_code=400, detail="User doesn't exist")
 
 
-def get_all_records(db: Session, username: str) -> List[entities.PlayRecord]:
-    # TODO: Implement this function
-    pass
+def get_all_records(db: Session, username: str) -> List[Type[PlayRecord]]:
+    records: List[Type[PlayRecord]] \
+        = db.query(PlayRecord).filter(PlayRecord.username == username).all()
+    return records
 
 
-def get_best_records(db: Session, username: str, underflow: int = 0) \
-        -> Tuple[List[entities.PlayRecord], List[entities.PlayRecord]]:
+def get_best_records(db: Session, username: str) -> List[Type[PlayRecord]]:
+    records: List[Type[PlayRecord]] \
+        = (db.query(PlayRecord)
+           .filter(PlayRecord.username == username)
+           .order_by(PlayRecord.rating.desc()).distinct(PlayRecord.song_level_id).all())
+    return records
+
+def get_best50_records(db: Session, username: str, underflow: int = 0) \
+        -> tuple[list[Type[PlayRecord]], list[Type[PlayRecord]]]:
     """
     Get best play records of a user. Returns a tuple. The first element is the list of records of old version (b35),
-    and the second element is the list of records of ne version (b15).
+    and the second element is the list of records of new version (b15).
     :param db: SQLAlchemy.orm Session
     :param username: the username of the user
     :param underflow: underflow records threshold
     :return: (list, list) like tuple
     """
-    # TODO: Implement this function
-    # Remark: it's possible to return a little more records i.e. underflow
-    pass
+    b35: List[Type[PlayRecord]] \
+        = (db.query(PlayRecord)
+           .filter(PlayRecord.username == username, PlayRecord.song_level.song.b15 is False)
+           .order_by(PlayRecord.rating.desc()).distinct(PlayRecord.song_level_id).limit(35 + underflow).all())
+    b15: List[Type[PlayRecord]] \
+        = (db.query(PlayRecord)
+           .filter(PlayRecord.username == username, PlayRecord.song_level.song.b15 is True)
+           .order_by(PlayRecord.rating.desc()).distinct(PlayRecord.song_level_id).limit(15 + underflow).all())
+
+    return b35, b15
--- backend/model/entities.py
+++ backend/model/entities.py
@@ -75,8 +75,8 @@ class PlayRecord(Base):
     song_level_id: Mapped[int] = mapped_column(ForeignKey('song_levels.song_level_id'))
     record_time: Mapped[datetime] = mapped_column()
     username: Mapped[str] = mapped_column(ForeignKey('prober_users.username'))
-    acc: Mapped[int] = mapped_column()
-    rating: Mapped[float] = mapped_column()
+    score: Mapped[int] = mapped_column()
+    rating: Mapped[float] = mapped_column()  # 便于查询 b50
 
     user: Mapped["User"] = relationship(back_populates='play_records')
     song_level: Mapped["SongLevel"] = relationship()
