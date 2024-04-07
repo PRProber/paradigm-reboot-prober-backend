8e07e578595b42f5268471d5aaa182648a34b600
IceLocke <lockeice@gmail.com>
2024-04-07 14:48:49 +0800

    添加 /songs/{id} 接口

--- backend/service/song.py
+++ backend/service/song.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
 
 from ..model import schemas, entities
 from ..crud import song as crud
@@ -38,6 +39,21 @@ def get_all_song_levels(db: Session):
         song_levels.extend(song_to_levels(song))
 
     return song_levels
+
+
+def get_single_song_by_id(db: Session, song_id: int):
+    song = crud.get_single_song_by_id(db, song_id)
+    if song is None:
+        raise HTTPException(status_code=404, detail="Song doesn't exist")
+    wrapped_song = util.SongInfo(song)
+    # 展开 level.difficulty.name，不然 schema 没法序列化
+    wrapped_song.song_levels = [
+        schemas.LevelInfo.model_validate({
+            key: getattr(level, key) if key != 'difficulty' else getattr(level, key).name
+            for key in schemas.LevelInfo.model_fields.keys()
+        }) for level in song.song_levels
+    ]
+    return wrapped_song
 
 
 def create_song(db: Session, song: schemas.SongCreate):
